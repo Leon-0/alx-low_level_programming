33c79c9f55dd41264b627b6f727d36cf8dcaa339
Jesuferanmi Omooniyi <jesuferanmiomoniyi@gmail.com>
2022-12-08 14:14:52 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(){
+/*
+ * main() : Entry point
+ *
+ * Description: Prints a string using the puts() function
+ *
+ * Return: function returns 0 alwayss
+ */
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
