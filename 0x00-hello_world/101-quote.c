4180eef48a8b42398f602e7ad1ecc4858c1d4188
Jesuferanmi Omoniyi <lyoncbe@gmail.com>
2022-12-09 16:36:43 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,18 @@
 #include <unistd.h>
 
 /**
- * main - Startung point
+ * main - Starting point
  *
  * Description: printing to console with write function
  *
- * return: returns an error of 1
+ * Return: returns an error of 1
  *
  */
 
 int main(void)
 {
-	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char word[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, sentence, 59);
+	write(1, word, 59);
 	return (1);
 }
