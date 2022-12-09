868711d1b1d2e97c191f7b518c05e43c99eaeff1
Jesuferanmi Omoniyi <lyoncbe@gmail.com>
2022-12-09 15:59:22 +0100

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+/*
+ * main - Startung point 
+ *
+ * Description: printing to console with write function 
+ *
+ * return: returns an error of 1
+ *
+ */
+
+int main(void)
+{
+	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, sentence, 59);
+	return(1);
+}
