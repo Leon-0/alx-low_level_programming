155b2ccc5f7f6da33096d4df0d8459741f997dfa
Jesuferanmi Omooniyi <jesuferanmiomoniyi@gmail.com>
2022-12-13 12:01:48 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Start of code
+ *
+ * Description: Prints the alphabet in lowercase followed by a new line
+ *
+ * Return: Always returns 0
+ */
+
+int main(void)
+{
+	int count;
+
+	for (count = 'a'; count <= 'z'; count++)
+	{
+		_putchar(count);
+	}
+	_putchar("/n");
+
+	return (0);
+}
