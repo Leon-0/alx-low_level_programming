dc927d468ef15ee02e53003b40ebc0c91d9a7286
Jesuferanmi Omooniyi <jesuferanmiomoniyi@gmail.com>
2022-12-13 11:42:20 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -14,11 +14,11 @@ int main(void)
 	int count;
 	char text[] = "_putchar";
 
-	for (count=0; count<8; count++)
+	for (count = 0; count < 8; count++)
 	{
 		_putchar(text[count]);
 	}
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
