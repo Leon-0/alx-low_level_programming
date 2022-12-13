a20d891f3c58b0c72ee69209519e522db88efef9
Jesuferanmi Omooniyi <jesuferanmiomoniyi@gmail.com>
2022-12-13 11:34:31 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Start of code
+ *
+ * Description: prints _putchar to the console
+ *
+ * Return: always returns 0
+ *
+ */
+
+int main(void)
+{
+	int count;
+	int text[] = "_putchar";
+
+	for (count=0; count<8; count++)
+	{
+		_putchar(text[count]);
+	}
+	_putchar('\n');
+
+	return(0);
+}
