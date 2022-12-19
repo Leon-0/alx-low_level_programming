40c73cd60e2c4ae8eaad311b87b56be1664125fc
Jesuferanmi Omoniyi <lyoncbe@gmail.com>
2022-12-19 20:45:58 +0100

    test

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int – swaps the values of two parameters
+ *
+ * @*a : first argument
+ * @*b : second argument
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = int *a;
+	int *a = int *b;
+	int *b = swap;
+}
