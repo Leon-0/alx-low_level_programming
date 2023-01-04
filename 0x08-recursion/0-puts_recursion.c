46aca17c9d378f3ed9b87f083eb9accfb2e3103e
Jesuferanmi Omoniyi <lyoncbe@gmail.com>
2023-01-04 13:27:35 +0100

    main

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,8 +5,10 @@
  * @s: pointer to the string
  */
 
-void _puts_recursion(const char *s) {
-    if (*s == '\0') {
+void _puts_recursion(char *s) 
+{
+    if (*s == '\0') 
+    {
 
         _putchar('\n');
         return;
