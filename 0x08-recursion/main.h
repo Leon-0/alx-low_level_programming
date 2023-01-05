f8abfd41456f2842c6648cebab77cad312e616dd
Leon-0 <36256754+Leon-0@users.noreply.github.com>
2023-01-05 02:56:58 +0100

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int square_root_helper(int x, int low, int high);
 int is_prime_number(int n);
 int square(int n, int val)
 int check_prime(int n, int resp);
