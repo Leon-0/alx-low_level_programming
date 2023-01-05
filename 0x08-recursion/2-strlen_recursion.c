6fb2750a462063b20c462601aa431c5f6f5f3a32
Jesuferanmi Omooniyi <jesuferanmiomoniyi@gmail.com>
2023-01-04 16:25:15 -0800

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,18 +3,18 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: Pointer to the characters of string
- *
+ * Return: returns the position of the next char
  */
 
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 {
-	return0;
+	return (0);
 }
 else
 {
-	return 1 + string_length(s + 1);
+	return (1 + string_length(s + 1));
 }
 
 }
