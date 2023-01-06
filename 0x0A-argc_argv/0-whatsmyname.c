dbc6aa219d0371131aef647ebc480233fc051356
Jesuferanmi Omooniyi <jesuferanmiomoniyi@gmail.com>
2023-01-06 12:11:27 -0800

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Prints program name 
+ * @argc: int
+ * @argv: list
+ * Return: zero
+ */
+
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
